97d58c3ccaa92ef3dfbb3ff5af90a700acba8f31
lnxxxxxxxx <leandro.luna25@hotmail.com>
2023-06-06 22:57:25 -0300

    new cors sin /

--- chatbot.py
+++ chatbot.py
@@ -34,7 +34,7 @@ chatbot = Chatbot()
 
 # Crear una aplicación Flask
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://chatbot-cat-ia.vercel.app/", "https://chat-mfp768m62-lnxxxxxxxx.vercel.app/"]}})
+CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://chatbot-cat-ia.vercel.app", "https://chat-mfp768m62-lnxxxxxxxx.vercel.app"]}})
 
 # Ruta para recibir y responder a las solicitudes POST
 @app.route('/chat', methods=['POST'])
